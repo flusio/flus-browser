95342aa7932c34455d66e8bedb4f0b61534d6726
Marien Fressinaud <marien+dev@fressinaud.fr>
2025-04-18 17:12:43 +0200

    dev: Provide an Icon component

--- src/application.js
+++ src/application.js
@@ -6,8 +6,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { i18n } from "./i18n.js";
 
+import Icon from "./components/Icon.vue";
+
 const app = createApp(App);
 
 app.use(i18n);
+app.component("Icon", Icon);
 
 app.mount("#app");
---
+++ src/components/Icon.vue
@@ -0,0 +1,22 @@
+<!-- This file is part of Flus Browser
+  -- SPDX-License-Identifier: AGPL-3.0-or-later
+  -->
+
+<template>
+    <svg :class="['icon', 'icon--' + name]" aria-hidden="true" width="36" height="36">
+        <use :xlink:href="iconHref"></use>
+    </svg>
+</template>
+
+<script setup>
+import icons from "url:npm:@flus/design/dist/icons.svg";
+
+const props = defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+});
+
+const iconHref = `${icons}#${props.name}`;
+</script>
