7555626a63dd332ff216e3d765220b419c252b67
Marien Fressinaud <marien+dev@fressinaud.fr>
2025-04-18 15:08:18 +0200

    dev: Remove the old files

--- src/configuration.js
+++
@@ -1,7 +0,0 @@
-// This file is part of Flus Browser
-// Copyright 2020-2025 Marien Fressinaud
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-export default {
-    app_endpoint: "https://app.flus.fr",
-};
