a9f7abc08dc32b9d9d75e89cc1cbee9be9abc47e
Marien Fressinaud <marien+dev@fressinaud.fr>
2025-04-18 15:13:52 +0200

    dev: Setup Vue.js

---
+++ src/application.js
@@ -0,0 +1,10 @@
+// This file is part of Flus Browser
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import { createApp } from "vue";
+
+import App from "./App.vue";
+
+const app = createApp(App);
+
+app.mount("#app");
