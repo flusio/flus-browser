d825c669cf45e8f4c9b415272cb02554784a0624
Marien Fressinaud <marien+dev@fressinaud.fr>
2025-05-15 17:53:10 +0200

    imp: Change header button to button--ghost

--- src/screens/Screen.vue
+++ src/screens/Screen.vue
@@ -10,7 +10,7 @@
             </div>
 
             <div>
-                <a v-if="menuOpened" href="#/" class="button button--icon button--small">
+                <a v-if="menuOpened" href="#/" class="button button--icon button--ghost button--small">
                     <Icon name="times" />
 
                     <span class="sr-only">
@@ -18,7 +18,7 @@
                     </span>
                 </a>
 
-                <a v-else href="#/menu" class="button button--icon button--small">
+                <a v-else href="#/menu" class="button button--icon button--ghost button--small">
                     <Icon name="menu" />
 
                     <span class="sr-only">
